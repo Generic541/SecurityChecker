7820ad7678779e960012f9d0652db0b6c520be96
unknown <qoo0302@naver.com>
2018-12-04 09:15:59 +0900

    Initial Commit

---
+++ SecurityChecker/app/src/main/java/kr/co/dothome/qoo0302/securitychecker/SecurityNews.java
@@ -0,0 +1,13 @@
+package kr.co.dothome.qoo0302.securitychecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecurityNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_news);
+    }
+}
