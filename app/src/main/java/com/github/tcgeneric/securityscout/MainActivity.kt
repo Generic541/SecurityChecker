39ed4a4636944061fccd940b505fa7fe5125f907
TC_G <qoo0302@naver.com>
2020-05-08 09:58:12 +0900

    Used PreferenceManager to store targets

--- app/src/main/java/com/github/tcgeneric/securityscout/MainActivity.kt
+++ app/src/main/java/com/github/tcgeneric/securityscout/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.WindowManager
 import android.widget.TextView
+import androidx.preference.PreferenceManager
 import com.github.tcgeneric.securityscout.securitythreat.DisplayDataProvider
 import com.github.tcgeneric.securityscout.securitythreat.SecurityThreatInfoProvider
 import com.github.tcgeneric.securityscout.securitythreat.TargetContextProvider
@@ -19,16 +20,22 @@ class MainActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
-        val source = findViewById<TextView>(R.id.source)
-        // TODO: Below code is stub
-        val target = TargetContextProvider.map["ahnlab"]!!
-        val future = SecurityThreatInfoProvider.getFuture(target.url, target.targetHTML)
-        source.post {
-            source.text = future.get(3, TimeUnit.SECONDS).display
-        }
+        val shared = PreferenceManager.getDefaultSharedPreferences(this)
+        if(shared.getString("target", null) == null)
+            shared.edit().putString("target", "ahnlab").apply()
+        reloadAlert(shared.getString("target", null)) // Non-nullability is guaranteed
     }
 
     private fun getId(type:String, name:String):Int {
         return resources.getIdentifier(name, type, "com.github.tcgeneric.securityscout")
+    }
+
+    private fun reloadAlert(id:String?) {
+        val source = findViewById<TextView>(R.id.source)
+        val target = TargetContextProvider.map[id]!!
+        val future = SecurityThreatInfoProvider.getFuture(target.url, target.targetHTML)
+        source.post {
+            source.text = future.get(3, TimeUnit.SECONDS).display
+        }
     }
 }
